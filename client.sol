6d637fe947fc13a3c20f8b6bfb662f769acaa6e1
Mathis Lecherf <mathis.lecherf@ynov.com>
2022-01-21 09:31:58 +0100

    Added numberOfClients + Patched addClient

--- client.sol
+++ client.sol
@@ -2,23 +2,30 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract NewClient{
 
-    event NewClient(uint clientId, string name, address wallet);
+    event NewClient(uint clientId, string name,string email, address wallet);
 
     struct Client{
         string name;
         string email;
         address wallet;
-
     }
 
     Client[] public clients;
     mapping (uint => address) owner;
     mapping (address => uint) public ownerCount;
 
-    function _addClient(string _name, address _wallet) {
-        uint id = clients.push(Client(_name, _wallet))-1);
+    function _addClient(string memory _name, string memory _email, address _wallet) public {
+        uint id = clients.push(Client(_name, _wallet, _email));
         owner[id] = msg.sender;
         ownerCount[msg.sender]++;
-        NewClient(id, _name, _wallet);
+        NewClient(id, _name, _email, _wallet);
+    }
+
+    function _numberOfClients(Client[] memory clientsArr) public returns(uint)  {
+        uint returnValue=0;
+        for(uint i = 0; i<clientsArr.length;i++){
+            returnValue++;
+        }
+        return returnValue;
     }
 }
